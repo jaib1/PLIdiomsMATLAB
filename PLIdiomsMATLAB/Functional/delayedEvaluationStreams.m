b79ff01f6c2fb569f79b9ccb82e3c1a63b7677d1
Jai Bhagat <jaib1@mit.edu>
2019-01-05 16:18:03 +0000

    fix typos

--- PLIdiomsMATLAB/Functional/delayedEvaluationStreams.m
+++ PLIdiomsMATLAB/Functional/delayedEvaluationStreams.m
@@ -2,7 +2,7 @@
 
 % A "stream" is an ostensibly infinite sequence of values, created from
 % thunking. The benefit of using a stream is that it can produce new values
-% forever without 1)needing to know beforehand when it should terminate and
+% forever without 1) needing to know beforehand when it should terminate and
 % therefore 2) allowing for memory and computation optimization
 
 % example: creating a stream of natural numbers (1,2,3...infinity)
@@ -11,5 +11,5 @@
 % number of the stream, which will return that element in the stream
 
 % does MATLAB provide any support for creating streams? couldn't figure out
-% a way to recursively incldue the anonymized function in its
+% a way to recursively include the anonymized function in its
 % definition
